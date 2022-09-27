3c8c7f6b16af552b9e3e0e4a8c7e8a41fba17feb
sebahub <groovegeier@gmail.com>
2022-09-27 10:43:18 +0200

    Add stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  publicRuntimeConfig: {
+    API_KEY: process.env.API_KEY,
+  },
+  css: ["@/assets/styles/main.sass"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@import "@/assets/styles/_variables.sass"',
+        },
+      },
+    },
+  },
+  buildModules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Poppins: true,
+      Roboto: true,
+    },
+    overwriting: true,
+  },
+});
